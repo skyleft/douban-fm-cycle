11d0261490ecde78959942ac4a82fabd71f87787
skyleft <skyleft123@gmail.com>
2015-02-13 13:58:39 +0800

    achieve the song repeat functionality

--- background.js
+++ background.js
@@ -1,8 +1,50 @@
+var prevSong;
+var isRepeat = false;
 chrome.webRequest.onBeforeRequest.addListener(
         function(details) {
-        	console.log(details);
-         return {cancel: "mp4".indexOf(details.url)!=-1}; 
+          //return {cancel: false}; 
+          var pat = /^.*mr\d{1}\.douban.com.*\.mp4$/;
+          var currentSong = details.url;
+          if(pat.test(currentSong)){
+            // chrome.storage.local.get('isRepeat',function(data){
+            //   if (data.isRepeat) {
+            //     chrome.storage.local.get('currentSong',function(data2){
+            //       if(data2.currentSong){
+            //         alert(data2.currentSong);
+            //         details.url = data2.currentSong;
+                    
+            //       }
+                    
+            //     })
+            //   }else{
+            //     saveCurrentSong(currentSong);
+            //   }
+            // });
+            if(isRepeat&&prevSong&&(prevSong!=currentSong)){
+              return {redirectUrl:prevSong};
+            }else{
+              prevSong = currentSong;
+            }
+          } 
        },
         {urls: ["*://*.douban.com/*"],types:["image", "object","other"]},
         ["blocking"]
 );
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+  if(message.isRepeat){
+    isRepeat = true;
+  }else{
+    isRepeat = false;
+  }
+  console.log('Got a message,turn '+isRepeat?'On':'Off' );
+});
+
+
+function saveCurrentSong(currentSong) {
+        if(currentSong){
+          chrome.storage.local.set({'currentSong': currentSong}, function() {
+            console.log("current song url is "+currentSong);
+          });
+        }
+}
--- script.js
+++ script.js
@@ -3,8 +3,26 @@ $(document).ready(function(){
 	//add the button
 	$("<div class='manipulator' id='repeat' />").appendTo("body");
 
+	var isRepeat = false;
 	$("#repeat").click(function(){
-		console.log("User choose repeat");
+		//chrome.storage.local.get('isRepeat',function(data){
+			if(isRepeat){
+				isRepeat = false;
+				chrome.runtime.sendMessage({'isRepeat': false});
+				console.log("Repeat Mode is Off!");
+				// chrome.storage.local.set({'isRepeat': false}, function() {
+		  //           console.log("Repeat Mode is Off!");
+		  //       });
+			}else{
+				isRepeat = true;
+				chrome.runtime.sendMessage({'isRepeat': true});
+				console.log("Repeat Mode is On!");
+				// chrome.storage.local.set({'isRepeat': true}, function() {
+    //         		console.log("Repeat Mode is On!");
+    //       		});
+			}
+		//})
+		
 	});
 
 });
